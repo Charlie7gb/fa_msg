6c52fbb6f959f4ff45450886b9bf69f15f5926fc
Charlie7gb <charliegbrael7@gmail.com>
2023-12-12 14:48:48 +0200

    master

---
+++ src/DB/connectDatabase.jsx
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectMongo = async () => {
+  try {
+
+    const { connection } = await mongoose.connect(process.env.MONGO_URL)
+    if (connection.readyState == 1) {
+      console.log("Database Connected")
+      return connection;
+    }
+  } catch (errors) {
+    return Promise.reject(errors)
+  }
+
+}
+export default connectMongo;
---
+++ src/model/Message.jsx
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+
+const { Schema } = mongoose;
+
+const messageSchema = new Schema({
+    NumID: { 
+        type: Number
+    },  
+    Name: {
+        type: String
+    },
+    Message: {
+        type: String
+    }
+})
+
+mongoose.models = {}
+export default mongoose.models.Message || mongoose.model('messages', messageSchema); 
