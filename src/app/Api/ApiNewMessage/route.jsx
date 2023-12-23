1cec5c86266636681e39747d8468d4bab8a0f939
Charlie7gb <charliegbrael7@gmail.com>
2023-12-23 13:45:14 +0200

    nn

--- src/app/Api/ApiNewMessage/route.jsx
+++ src/app/Api/ApiNewMessage/route.jsx
@@ -29,11 +29,11 @@ export async function GET() {
       const ListMessage = await Message.findOne(query);
       //console.log(ListMessage)*/
       const client = await connectMongo();
-      const db = client.db("FAMessage");
+      
       
       //console.log(ListMessage)
   
-      return NextResponse.json({ msgO :db }, { status: 200 })
+      return NextResponse.json({ msgO :client }, { status: 200 })
     } catch (error) {
       return NextResponse.json({ error: "Error in the Connection **"}, { status: 500 })
     }
