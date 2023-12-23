f1fb520a88731d5142646f1918165d6e5cf60b97
Charlie7gb <charliegbrael7@gmail.com>
2023-12-23 11:34:20 +0200

    Update_1

--- src/DB/connectDatabase.jsx
+++ src/DB/connectDatabase.jsx
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongo = async () => {
   try {
-
+  //MONGO_URL = 'mongodb+srv://elie123ayoub:NIPTgZUETenvGh5N@cluster0.exlopje.mongodb.net/'
     const { connection } = await mongoose.connect(process.env.MONGO_URL)
     if (connection.readyState == 1) {
       console.log("Database Connected")
