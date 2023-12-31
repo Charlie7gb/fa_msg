dd64e79f2351eebe153ac978042351443244262f
Charlie7gb <charliegbrael7@gmail.com>
2023-12-31 14:13:27 +0200

    up_39

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -2,8 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="container-fluid d-flex justify-content-center">
-      <div className="justify-content-center align-items-center">
+    <div className="container-fluid d-flex justify-content-center mt-5">
+      <div className="justify-content-center align-items-center mt-5">
         <SignIn />
       </div>
     </div>
--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -2,8 +2,8 @@ import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
   return(
-    <div className="container-fluid d-flex justify-content-center">
-      <div className="justify-content-center align-items-center">
+    <div className="container-fluid d-flex justify-content-center mt-5">
+      <div className="justify-content-center align-items-center mt-5">
       <SignUp />
       </div>
     </div>
